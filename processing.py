87b6969746a41c6cd59782d0495f18506c2415fa
Sergey77-MX <melnikov_77@mail.ru>
2024-09-05 04:00:44 +0700

    add_filter_by_state_func

--- processing.py
+++ processing.py
@@ -1,6 +1,9 @@
-def filter_by_state():
-    pass
-
+def filter_by_state(list_dict, state):
+    list_sort = []
+    for i in list_dict:
+        if i['state'] == state:
+            list_sort.append(i)
+    return list_sort
 
 
 def sort_by_date():
